cc4320b7354e5326502557e2fac30fe565714b0d
Tyler Cookson <tcookson0805@gmail.com>
2017-08-11 14:11:08 -0700

    created folder and some common components

---
+++ src/components/common/Input.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = (props) => {
+	return (
+		<View style={styles.containerStyle}>
+			<Text style={styles.labelStyle}>
+				{props.label}
+			</Text>
+			<TextInput style={styles.inputStyle} />
+		</View>
+	);
+};
+
+const styles = {
+	containerStyle: {
+		height: 40,
+		flexDirection: 'row',
+		alignItems: 'center',
+		flex: 1
+	},
+	labelStyle: {
+		flex: 1,
+		fontSize: 18,
+		paddingLeft: 20
+	},
+	inputStyle: {
+		fontSize: 18,
+		lineHeight: 23,
+		flex: 3,
+		color: '#000',
+		paddingRight: 5,
+		paddingLeft: 5,
+	}
+};
+
+export { Input };
---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+export * from './Card';
+export * from './CardSection';
+export * from './Input';
+export * from './Button';
+export * from './Header';

7e94b71586890c1e3950dd39c23d05b387e1ec69
Tyler Cookson <tcookson0805@gmail.com>
2017-08-11 16:03:22 -0700

    wired up redux

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
+import reducers from './reducers';
+
 const App = () => {
 	return (
-		<View>
-			<Header />
-			<LoginForm />
-		</View>
+		<Provider store={createStore(reducers)}>
+			<View>
+				<Header />
+				<LoginForm />
+			</View>
+		</Provider>
 	);
 };
 
